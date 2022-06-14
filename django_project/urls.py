a30dd4c17c1dfd8ec8d3dc9db08ff4376d776703
Ronald Lopez <lopez.j.ronald@gmail.com>
2022-06-14 00:28:03 -0700

    add user authentication

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("posts.urls")),
+    path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/v1/", include("posts.urls")),
+    path("admin/", admin.site.urls),
 ]
